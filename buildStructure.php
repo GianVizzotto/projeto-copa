2e03879bac9dc983401cb86c27f614002181f053
GianVizzotto <gfvizzotto@gmail.com>
2014-06-07 21:57:10 -0300

    Criada função para ler dados da planilha de empresas e grupos e inserir no wp

--- buildStructure.php
+++ buildStructure.php
@@ -7,7 +7,12 @@ $connection = new createConnection(); //i created a new object
 
 $p = new buildStructure();
 
-$p->create_posts_network_power( $connection->connectToDatabase() );
+if($argv[1] == 'groups') {
+	$p->insert_from_empresas( $connection->connectToDatabase() );
+} else {
+	$p->create_posts_network_power( $connection->connectToDatabase() );	
+}
+
 
 class buildStructure {
 	
@@ -57,6 +62,10 @@ class buildStructure {
 		return mysql_db_query('vaimudar', 'select * from custom_networks where key_id = '.$key_id.' and is_key = 0', $conn);
 	}
 	
+	function search_by_empresa($conn, $group_id) {
+		return mysql_db_query('vaimudar', 'select * from empresa where grupo_id = '.$group_id, $conn);
+	}
+	
 	/*function search_by_slug() {
 		$args = array(
 			'name' 			=> $the_slug,
@@ -69,6 +78,34 @@ class buildStructure {
 		
 		print_r($post); die;
 	}*/
-	
+	function insert_from_empresas ($conn) {
+		$groups = mysql_db_query('vaimudar', 'select * from grupo_economico', $conn);
+		
+		while($row = mysql_fetch_assoc($groups, MYSQL_ASSOC)) {
+			
+			if( term_exists($row['name']) ) {
+				continue;
+			}
+			$category = wp_insert_term( ucwords( str_replace( array('_','-'), ' ', $row['name'] ) ), 'redes-de-poder' );
+			
+			$empresa = $this->search_by_empresa($conn,$row['id']);
+			
+			while($r = mysql_fetch_assoc($empresa, MYSQL_ASSOC)) {
+				$post = array (
+					'post_content' 	=> '',
+					'post_title'	=> ucwords( str_replace( array('_','-'), ' ', $r['name'] ) ),
+					'post_type'		=> 'empresas',
+					'post_status'	=> 'publish',
+					'tax_input'		=> array('redes-de-poder') 
+				);
+				
+				$post_id = wp_insert_post($post);
+				
+				wp_set_post_terms( $post_id, array($category['term_id']), 'redes-de-poder', true );
+				add_post_meta($post_id, 'cnpj', $r['cnpj']);
+			}
+		}
+		
+	}
 }
 ?>
